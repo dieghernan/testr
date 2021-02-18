58ff2a4fb1ced3c352003960e67fd8ba2f10b9f2
dieghernan <dirgego.hernangomezherrero@gmail.com>
2021-02-18 12:39:24 +0100

    Go

--- R/prepare_renv.R
+++ R/prepare_renv.R
@@ -1,8 +1,12 @@
 renv::init()
-
+renv::dependencies()
 renv::snapshot()
 renv::status()
 renv::install("magick")
 renv::clean()
-
+renv::restore()
 library(magick)
+
+renv::paths$library()
+
+Sys.getenv("RENV_PATHS_ROOT")
